172bafce960bdcc8eb2d1e1c4c816feb23dacd6b
JPG <jpg.soporte@gmail.com>
2022-02-17 18:33:55 -0400

    CountingValleys (JS)

---
+++ CountingValleys/index.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+function countingValleys(steps, path) {
+    // Write your code here
+    let valleys = 0;
+    let level = 0;
+    let beendown = false; 
+    for(let i=0;i<steps;i++){
+    	level += path[i] == "D" ? -1 : 1;
+    	if(level == -1){
+    		beendown = true;
+    	}else if(level == 0){
+    		if(beendown)++valleys;
+    		beendown = false;
+    	}
+
+
+
+
+    }
+    return valleys;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream('output');
+
+    const steps = parseInt(readLine().trim(), 10);
+
+    const path = readLine();
+
+    const result = countingValleys(steps, path);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
